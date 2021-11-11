d0ab98d088cb26741c44c9be4f0172e0d1513280
Kevin McAleer <kevinmcaleer@gmail.com>
2021-11-11 15:56:04 +0000

    Update bullseye_upgrade.sh

--- bullseye_upgrade.sh
+++ bullseye_upgrade.sh
@@ -4,6 +4,9 @@
 
 echo "Kev's Powertools - Raspberry Pi OS Buster to Bullseye installation Script"
 
+# fix the wifi bug
+echo ‘rfkill unblock 0’ >> /etc/rc.local
+
 # Replace buster with bullseye
 sudo sed -i 's/buster/bullseye/g' /etc/apt/sources.list
 sudo sed -i 's/buster/bullseye/g' /etc/apt/sources.list.d/raspi.list
@@ -16,4 +19,3 @@ sudo install gcc-8-base
 sudo apt full-upgrade -y
 
 # Add updater to menu panel
-echo ‘rfkill unblock 0’ >> /etc/rc.local
